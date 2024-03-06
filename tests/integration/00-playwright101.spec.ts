937bec39434a5d17b951e7aedeb3ea2d4d24fce9
buraksimsekgit <simsek.bl@gmail.com>
2024-03-05 21:20:58 -0600

    first commit

---
+++ tests/integration/00-playwright101.spec.ts
@@ -0,0 +1,33 @@
+import { test, chromium } from "@playwright/test";
+
+// This will trigget the test runner.
+// test("Playwright 101 - Test Case", () => {
+//   // Test to be executed
+// });
+
+// Test runner will trigger the browser context
+// using the { page } fixture
+test("Playwright 101 - Test Case 2", ({ page }) => {
+  // Test to be executed
+});
+
+// Marks a function as asynchronous using 'async'
+// meaning it might take some time to complete.
+test("Playwright 101 - Test Case 3", async ({ page }) => {
+  // The await keyword pauses function exection until a Promise is resolved,
+  // ensuring code runs only after the Promise is fulfilled or rejected.
+  await page.goto("https://techglobal-training.com");
+});
+
+test.only('Playwright 101 - Test Case 4', async () => {
+
+    const browser = await chromium.launch()
+    const context = await browser.newContext()
+    const page = await context.newPage()
+
+    await page.goto("https://techglobal-training.com");
+
+    await page.close()
+})
+
+
