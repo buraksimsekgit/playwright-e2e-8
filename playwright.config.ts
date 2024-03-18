bc1775b28a3604c7c11bd4e0fbc89d2a0d3651e4
buraksimsekgit <simsek.bl@gmail.com>
2024-03-18 17:34:14 -0500

    junit

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   reporter: [
     ['html', { open: 'never' }],
     // ["json", { outputFile: "json/results.json" }],
-    ["junit"],
+    ["junit", { outputFile: "junit/results.xml" }],
     ['list'],
   ],
 
